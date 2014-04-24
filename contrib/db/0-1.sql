63ed381c2c6e5659cf1b5a54cb99a2aeeb880a54
Adam Harwell <flux.adam@gmail.com>
2014-04-24 14:49:37 -0500

    Fixes and such to make it actually load up.

--- contrib/db/0-1.sql
+++ contrib/db/0-1.sql
@@ -11,46 +11,46 @@ CREATE TABLE `member` (
     `weight` int(11) DEFAULT NULL,
     `status` varchar(32) DEFAULT NULL,
     PRIMARY KEY (`id`),
-    CONSTRAINT  `fk_m_condition` FOREIGN KEY (condition) REFERENCES `enum_member_condition`(name),
-    CONSTRAINT `fk_m_status` FOREIGN KEY (status) REFERENCES `enum_member_status`(name),
-    CONSTRAINT `fk_m_pool` FOREIGN KEY (pool_id) REFERENCES pool(id)
+    CONSTRAINT `fk_m_condition` FOREIGN KEY (`condition`) REFERENCES `enum_member_condition`(name),
+    CONSTRAINT `fk_m_status` FOREIGN KEY (`status`) REFERENCES `enum_member_status`(name),
+    CONSTRAINT `fk_m_pool` FOREIGN KEY (`pool_id`) REFERENCES pool(id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `pool`;
 CREATE TABLE `pool` (
-    `id` int(32) NOT NULL,
-    `health_monitor_id` int(32) NOT NULL,
-    `tenant_id` varchar(128) DEFAULT NULL
+    `id` int(11) NOT NULL,
+    `health_monitor_id` int(11) NOT NULL,
+    `tenant_id` varchar(128) DEFAULT NULL,
     `name` varchar(128) DEFAULT NULL,
     `subnet_id` varchar(128) DEFAULT NULL,
-    `ssl_encrypt_id` int(32) DEFAULT NULL,
+    `ssl_encrypt_id` int(11) DEFAULT NULL,
     `session_persistence` varchar(32) DEFAULT NULL,
     `algorithm` varchar(32) DEFAULT NULL,
     PRIMARY KEY (`id`),
-    CONSTRAINT `fk_p_sp` FOREIGN KEY (session_persistence) REFERENCES enum_pool_session_persistence(id),
-    CONSTRAINT `fk_p_algo` FOREIGN KEY (algorithm) REFERENCES enum_pool_algorithm(id),
-    CONSTRAINT `fk_p_ssl_encrypt` FOREIGN KEY (ssl_encrypt_id) REFERENCES ssl_encrypt(id),
-    CONSTRAINT `fk_p_hm_id` FOREIGN KEY (health_monitor_id) REFERENCES health_monitor(id)
+    CONSTRAINT `fk_p_sp` FOREIGN KEY (`session_persistence`) REFERENCES enum_pool_session_persistence(name),
+    CONSTRAINT `fk_p_algo` FOREIGN KEY (`algorithm`) REFERENCES enum_pool_algorithm(name),
+    CONSTRAINT `fk_p_ssl_encrypt` FOREIGN KEY (`ssl_encrypt_id`) REFERENCES ssl_encrypt(id),
+    CONSTRAINT `fk_p_hm_id` FOREIGN KEY (`health_monitor_id`) REFERENCES health_monitor(id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `health_monitor`;
 CREATE TABLE `health_monitor` (
-    `id` int(32) NOT NULL,
+    `id` int(11) NOT NULL,
     `type` varchar(32) DEFAULT NULL,
-    `delay` int(32) DEFAULT NULL,
-    `timeout` int(32) DEFAULT NULL,
-    `attempts_before_deactivation` int(32) DEFAULT NULL,
+    `delay` int(11) DEFAULT NULL,
+    `timeout` int(11) DEFAULT NULL,
+    `attempts_before_deactivation` int(11) DEFAULT NULL,
     `status_regex` varchar(128) DEFAULT NULL,
     `body_regex` varchar(128) DEFAULT NULL,
     `host_header` varchar(256) DEFAULT NULL,
     `path` varchar(128) DEFAULT NULL,
     PRIMARY KEY (`id`),
-    CONSTRAINT `fk_hm_type` FOREIGN KEY (type) REFERENCES enum_health_monitor_type(id)
+    CONSTRAINT `fk_hm_type` FOREIGN KEY (type) REFERENCES enum_health_monitor_type(name)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `rule`;
 CREATE TABLE `rule` (
-    `id` int(32) NOT NULL,
+    `id` int(11) NOT NULL,
     `type` varchar(32) DEFAULT NULL,
     `condition` varchar(128) DEFAULT NULL,
     PRIMARY KEY (`id`),
@@ -59,47 +59,47 @@ CREATE TABLE `rule` (
 
 DROP TABLE IF EXISTS `ssl_decrypt`;
 CREATE TABLE `ssl_decrypt` (
-    `id` int(32) NOT NULL,
-    `tenant_id` varchar(128) DEFAULT NULL
+    `id` int(11) NOT NULL,
+    `tenant_id` varchar(128) DEFAULT NULL,
     `barbican_uuid` varchar(128) DEFAULT NULL,
     `enabled` int(1) DEFAULT 0,
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `ssl_encrypt`;
 CREATE TABLE `ssl_encrypt` (
-    `id` int(32) NOT NULL,
-    `tenant_id` varchar(128) DEFAULT NULL
+    `id` int(11) NOT NULL,
+    `tenant_id` varchar(128) DEFAULT NULL,
     `barbican_uuid` varchar(128) DEFAULT NULL,
     `enabled` int(1) DEFAULT 0,
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `tls_certificate`;
-CREATE TABLE `ssl_encrypt` (
-    `id` int(32) NOT NULL,
-    `tenant_id` varchar(128) DEFAULT NULL
+CREATE TABLE `tls_certificate` (
+    `id` int(11) NOT NULL,
+    `tenant_id` varchar(128) DEFAULT NULL,
     `barbican_uuid` varchar(128) DEFAULT NULL,
     `simple_certificate_data` varchar(256) DEFAULT NULL,
-    PRIMARY KEY (`id`),
+    PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `vip`;
 CREATE TABLE `vip` (
-    `id` int(32) NOT NULL,
-    `tenant_id` varchar(128) DEFAULT NULL
+    `id` int(11) NOT NULL,
+    `tenant_id` varchar(128) DEFAULT NULL,
     `subnet_id` varchar(32) DEFAULT NULL,
     `type` varchar(32) DEFAULT NULL,
     PRIMARY KEY (`id`),
-    CONSTRAINT `fk_v_type` FOREIGN KEY (session_persistence) REFERENCES enum_vip_type(id)
+    CONSTRAINT `fk_v_type` FOREIGN KEY (`type`) REFERENCES enum_vip_type(name)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `load_balancer`;
 CREATE TABLE `load_balancer` (
-    `id` int(32) NOT NULL AUTO_INCREMENT,
-    `tenant_id` varchar(128) DEFAULT NULL
-    `ssl_decrypt_id` int(32) DEFAULT NULL,
-    `tls_certificate_id` int(32) DEFAULT NULL,
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `tenant_id` varchar(128) DEFAULT NULL,
+    `ssl_decrypt_id` int(11) DEFAULT NULL,
+    `tls_certificate_id` int(11) DEFAULT NULL,
     `name` varchar(128) DEFAULT NULL,
     `content_switching` int(1) DEFAULT 0,
     `port` int(11) DEFAULT NULL,
@@ -107,16 +107,14 @@ CREATE TABLE `load_balancer` (
     `status` varchar(32) DEFAULT NULL,
     PRIMARY KEY (`id`),
     CONSTRAINT `fk_g_status` FOREIGN KEY (status) REFERENCES `enum_lbaas_status`(name),
-    CONSTRAINT `fk_g_ut` FOREIGN KEY (update_type) REFERENCES `enum_update_type`(name),
-    CONSTRAINT  `fk_g_algo` FOREIGN KEY (algorithm) REFERENCES `enum_glb_algorithm`(name),
-    CONSTRAINT `fk_p_ssl_decrypt` FOREIGN KEY (ssl_decrypt_id) REFERENCES ssl_decrypt(id)
+    CONSTRAINT `fk_p_ssl_decrypt` FOREIGN KEY (ssl_decrypt_id) REFERENCES ssl_decrypt(id),
     CONSTRAINT `fk_p_tls_cert` FOREIGN KEY (tls_certificate_id) REFERENCES tls_certificate(id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `lb_vip`;
 CREATE TABLE `lb_vip` (
-    `lb_id` int(32) NOT NULL,
-    `vip_id` varchar(128) DEFAULT NULL,
+    `lb_id` int(11) NOT NULL,
+    `vip_id` int(11) DEFAULT NULL,
     PRIMARY KEY (`lb_id`, `vip_id`),
     CONSTRAINT `fk_v_lb_id` FOREIGN KEY (lb_id) REFERENCES load_balancer(id),
     CONSTRAINT `fk_v_vip_id` FOREIGN KEY (vip_id) REFERENCES vip(id)
@@ -124,19 +122,19 @@ CREATE TABLE `lb_vip` (
 
 DROP TABLE IF EXISTS `lb_pool`;
 CREATE TABLE `lb_pool` (
-    `lb_id` int(32) NOT NULL,
-    `pool_id` varchar(128) DEFAULT NULL,
+    `lb_id` int(11) NOT NULL,
+    `pool_id` int(11) DEFAULT NULL,
     `default` int(1) DEFAULT 0,
-    PRIMARY KEY (`lb_id`, `vip_id`),
+    PRIMARY KEY (`lb_id`, `pool_id`),
     CONSTRAINT `fk_p_lb_id` FOREIGN KEY (lb_id) REFERENCES load_balancer(id),
     CONSTRAINT `fk_p_pool_id` FOREIGN KEY (pool_id) REFERENCES pool(id)
 ) ENGINE=InnoDB;
 
 DROP TABLE IF EXISTS `lb_pool_rule`;
 CREATE TABLE `lb_pool_rule` (
-    `lb_id` int(32) NOT NULL,
-    `pool_id` int(32) DEFAULT NULL,
-    `rule_id` int(32) DEFAULT NULL,
+    `lb_id` int(11) NOT NULL,
+    `pool_id` int(11) DEFAULT NULL,
+    `rule_id` int(11) DEFAULT NULL,
     PRIMARY KEY (`lb_id`, `pool_id`, `rule_id`),
     CONSTRAINT `fk_lbpr_lb_id` FOREIGN KEY (lb_id) REFERENCES load_balancer(id),
     CONSTRAINT `fk_lbpr_pool_id` FOREIGN KEY (pool_id) REFERENCES pool(id),
@@ -194,6 +192,13 @@ CREATE TABLE `enum_health_monitor_type` (
 
 DROP TABLE IF EXISTS `enum_pool_algorithm`;
 CREATE TABLE `enum_pool_algorithm` (
+    `name` varchar(32) DEFAULT NULL,
+    `description` varchar(128) DEFAULT NULL,
+    PRIMARY KEY (`name`)
+) ENGINE=InnoDB;
+
+DROP TABLE IF EXISTS `enum_pool_session_persistence`;
+CREATE TABLE `enum_pool_session_persistence` (
     `name` varchar(32) DEFAULT NULL,
     `description` varchar(128) DEFAULT NULL,
     PRIMARY KEY (`name`)
