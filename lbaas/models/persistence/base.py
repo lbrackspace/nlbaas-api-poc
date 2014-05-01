c544256c6e321cb0affc102664c5c9f8f5549334
Brandon Logan <brandon.logan@rackspace.com>
2014-04-30 20:49:20 -0500

    Fixed recursive dicting of models.

--- lbaas/models/persistence/base.py
+++ lbaas/models/persistence/base.py
@@ -17,15 +17,25 @@ class BaseModel(object):
         return self.get_pub_vars()
 
     def get_pub_vars(self):
+        accepted_types = (int, long, float, str, unicode, bool, dict, list,
+                          BaseModel)
+        ignore_vars = ['TAG']
         pub_vars = {}
-        # attrs = dir(self)
-        attrs = vars(self)
+        if hasattr(self, '_child_classes'):
+            recurse_classes = self._child_classes
+        else:
+            recurse_classes = ()
+        attrs = dir(self)
         for attr_name in attrs:
-            if getattr(self, attr_name) is None:
+            attr = getattr(self, attr_name)
+            if attr is None or attr_name in ignore_vars or \
+                    hasattr(attr, '__call__'):
                 continue
             if attr_name[0] != '_':
-                if isinstance(getattr(self, attr_name), BaseModel):
-                    pub_vars[attr_name] = getattr(self, attr_name).to_dict()
-                else:
-                    pub_vars[attr_name] = getattr(self, attr_name)
+                attr = getattr(self, attr_name)
+                if isinstance(attr, accepted_types):
+                    if isinstance(attr, recurse_classes):
+                        pub_vars[attr_name] = attr.to_dict()
+                    else:
+                        pub_vars[attr_name] = attr
         return pub_vars
